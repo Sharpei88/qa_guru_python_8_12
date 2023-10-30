470718549ce91ec4824e1cc7253abe45d1a26519
sharov_vitalii <fly44088@gmail.com>
2023-10-30 22:54:23 +0300

    изменения в conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ from selene import browser
 from utils import attach
 
 
-@pytest.fixture(scope='function', autouse=True)
+@pytest.fixture(scope='function')
 def setup_browser(request):
     options = Options()
     selenoid_capabilities = {
@@ -20,7 +20,7 @@ def setup_browser(request):
     }
     options.capabilities.update(selenoid_capabilities)
     driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@sefаlenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
         options=options
     )
 
